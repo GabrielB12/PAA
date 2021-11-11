4f38da417706f6265e4e589c7f08174561e067ff
GabrielB12 <51236328+GabrielB12@users.noreply.github.com>
2021-11-11 00:40:04 -0300

    Delete exercicio3.c

--- PAA/exercicio3.c
+++
@@ -1,6 +0,0 @@
-/*
-    professor, eu estava utilizando antes funcoes externas à funcao principal, que monta os casais
-    porém estava encotrando erro nos ponteiros, então coloquei elas dentro da mesma funcao maior,
-    porém, ao remover as funcoes externas no judge um dos casos aparece como "killed", entao deixei elas
-    no codigo mesmo
-*/
